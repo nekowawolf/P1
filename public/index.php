10e2e75a05b9fda6b9668b6fa9843bbdbceeee6d
Andika MF <irukainu20@gmail.com>
2024-04-21 21:22:31 +0700

    1

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Donasi</title>
+    <link rel="shortcut icon" href="/public/img/logo.jpg" />
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+  <body>
+
+  <style>
+  body {
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: blue; /* Ubah warna latar belakang sesuai kebutuhan */
+  }
+  
+  .donasi {
+    font-size: 4em; /* Ukuran font besar */
+    color: white; /* Warna teks putih agar kontras dengan latar belakang */
+  }
+</style>
+</head>
+<body>
+  <div class="donasi">Donasi awkoakwoaw</div>
+
+  </body>
+</html>
