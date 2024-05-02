8b75b8916dbc44378a3a504695e9daa26fcb515d
nekowawolf <irukainu20@gmail.com>
2024-05-02 07:19:43 +0700

    322

--- public/index.php
+++ public/index.php
@@ -25,5 +25,6 @@
 </header>
 
 
+
   </body>
 </html>
--- public/register.php
+++ public/register.php
@@ -9,11 +9,7 @@
   <body class="bg-gray-50 dark:bg-gray-900 flex justify-center items-center h-screen">
     <div class="container mx-auto">
       <div class="flex flex-col lg:flex-row w-10/12 lg:w-8/12 bg-white rounded-xl mx-auto shadow-lg overflow-hidden">
-        <div class="w-full lg:w-1/2 flex flex-col items-center justify-center p-12 bg-no-repeat bg-cover bg-center" style="background-image: url('');">
-          <h1 class="text-white text-3xl mb-3">Welcome</h1>
-          <div>
-            <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean suspendisse aliquam varius rutrum purus maecenas ac <a href="#" class="text-purple-500 font-semibold">Learn more</a></p>
-          </div>
+        <div class="w-full lg:w-1/2 flex flex-col items-center justify-center p-12 bg-no-repeat bg-cover bg-center" style="background-image: url('https://mtypks.org/wp-content/uploads/2018/10/mtyp-name-your-own-price-donation-image.png');">
         </div>
         <div class="w-full lg:w-1/2 py-16 px-12">
           <h2  class="text-3xl mb-4">Register</h2>
