093bbe2eae0c8cf94d95eac700020b9ca3693347
nekowawolf <irukainu20@gmail.com>
2024-05-02 08:00:28 +0700

    566

--- public/index.php
+++ public/index.php
@@ -24,7 +24,27 @@
     </div>
 </header>
 
+<section id="home" class="py-20 bg-white">
+    <div class="container mx-auto text-center">
+        <h1 class="text-4xl font-bold uppercase mb-8">Satu langkah kecil, berdampak besar</h1>
+        <p class="text-lg text-gray-600 mx-auto max-w-lg">Satu Langkah Kecil, Dampak Besar adalah sebuah inisiatif yang memperlihatkan bagaimana setiap tindakan kebaikan yang sederhana dapat memiliki dampak yang besar dalam kehidupan orang lain</p>
+        <a href="login.php" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-block mt-16">Donasi Sekarang</a>
+    </div>
+</section>
 
+<section id="about" class="py-20 bg-gray-200">
+    <div class="container mx-auto">
+        <div class="flex flex-col lg:flex-row items-center justify-between">
+            <div class="lg:w-1/2 lg:mr-10 text-center lg:text-left mx-16">
+                <h2 class="text-3xl font-bold mb-4">About Us</h2>
+                <p class="text-lg text-gray-700">Kami adalah sebuah platform yang didedikasikan untuk membantu mereka yang membutuhkan. Di sini, kami percaya bahwa setiap individu memiliki kekuatan untuk membuat perubahan positif dalam dunia ini. Dengan tekad untuk membangun sebuah komunitas yang peduli dan berempati, kami hadir untuk memfasilitasi keterlibatan Anda dalam memberikan bantuan kepada mereka yang membutuhkan.</p>
+            </div>
+            <div class="lg:w-2/6 mr-28 mt-10 lg:mt-0">
+                <img src="https://mtypks.org/wp-content/uploads/2018/10/mtyp-name-your-own-price-donation-image.png" alt="About Us" class="rounded-lg shadow-lg">
+            </div>
+        </div>
+    </div>
+</section>
 
   </body>
 </html>
